aea508b9acdd8c45528b8b757fbedbb10e7b79d3
unmeshvrije <unmesh.joshi126@gmail.com>
2017-01-11 18:15:50 +0100

    Updated write-pickle file to read text file and convert it to a pickle format

--- write_pickle.py
+++ write_pickle.py
@@ -2,14 +2,16 @@ import pickle, pprint
 import sys
 
 def main(datafile):
-    with open(datafile,'rb')as fin:
-        data = pickle.load(fin)
+    f = open(datafile, 'r')
+    data = eval(f.read())
+    print (len(data['entities']))
+    print ("%3d training tuples\n" % (len(data['train_subs'])))
+    with open('pickle-db.pkl','wb')as fout:
+        pickle.dump(data, fout, -1)
 
-    pprint.pprint(data)
 
 if __name__=='__main__':
     if len(sys.argv) != 2:
-        print "Pickle database file must be given as an argument."
+        print ("Pickle database file must be given as an argument.")
         sys.exit()
     main(sys.argv[1])
-
