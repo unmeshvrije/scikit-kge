6c11939bb10e1cfbe1c83a621e85982a37eb9a81
unmeshvrije <unmesh.joshi126@gmail.com>
2017-02-07 17:46:43 +0100

    Randomly generate training and test sets. Write labels in the edge list if they exist

--- snap-to-pyobj.py
+++ snap-to-pyobj.py
@@ -1,6 +1,6 @@
 import pickle, pprint
 import sys
-
+from random import shuffle
 #
 #relations
 #entities
@@ -13,10 +13,10 @@ def main(datafile):
     with open(datafile, 'r') as f:
         lines = f.readlines()
         cntEdges = len(lines)
-        if cntEdges < 1000:
+        if cntEdges < 100000:
             percentageTraining = 80
         else:
-            percentageTraining = 99.8
+            percentageTraining = 99
         cntTraining = int((cntEdges * percentageTraining)/100)
         cntTest = int((cntEdges * (float(100 - percentageTraining)/2)) / 100)
         cntValid = cntTest
@@ -26,22 +26,36 @@ def main(datafile):
         print ("Total triples : %d\n" % (cntEdges))
 
         entities_set = set()
+        relations_set = set()
         entities_map = {}
+        relations_map = {}
         entities = "u'entities': ["
-        relations = "u'relations': [u'related_to'],"
+        relations = "u'relations':["
         test = "u'test_subs':["
         trains = "u'train_subs':["
         valid = "u'valid_subs':["
 
         counter = 0
         identifier = 0
+        relationId = 0
+
+        shuffle(lines)
+
         for index, pair in enumerate(lines):
             counter += 1
             if len(pair.split()) < 2:
                 print ("Line [%d] does not represent an edge" % (index))
                 continue
-            fromNode = pair.split()[0]
-            toNode = pair.split()[1]
+            parts = pair.split()
+            fromNode = parts[0]
+            toNode = parts[1]
+            if (len(parts) > 2):
+                edgeLabel = parts[2]
+                if edgeLabel not in relations_set:
+                    relations_set.add(edgeLabel)
+                    relations_map[edgeLabel] = relationId
+                    relationId +=1
+
             if fromNode not in entities_set:
                 entities_set.add(fromNode)
                 entities_map[fromNode] = identifier
@@ -58,27 +72,38 @@ def main(datafile):
         fwalks = open(datafile + '.0-based-entitiy-ids.edgelist', 'w')
         print ("# of identifiers (entities) = %d" % (identifier))
         for index, pair in enumerate(lines):
-            fromNode = pair.split()[0]
-            toNode = pair.split()[1]
+            parts = pair.split()
+            fromNode = parts[0]
+            toNode = parts[1]
+            edgeLabel = 0
+            if len(parts) > 2:
+                edgeLabel = parts[2]
 
             fwalks.write(str(entities_map[fromNode]) + " " + str(entities_map[toNode]) + "\n")
 
             if index < cntTraining-1:
-                trains += "(" + str(entities_map[fromNode]) + "," + str(entities_map[toNode]) + ",0),\n"
+                trains += "(" + str(entities_map[fromNode]) + "," + str(entities_map[toNode]) + ","+str(edgeLabel)+"),\n"
             elif index == cntTraining - 1:
-                trains += "(" + str(entities_map[fromNode]) + "," + str(entities_map[toNode]) + ",0)],"
+                trains += "(" + str(entities_map[fromNode]) + "," + str(entities_map[toNode]) + ","+str(edgeLabel)+")],"
             elif index < testIndex-1:
-                test += "(" + str(entities_map[fromNode]) + "," + str(entities_map[toNode]) + ",0),\n"
+                test += "(" + str(entities_map[fromNode]) + "," + str(entities_map[toNode]) + ","+str(edgeLabel)+"),\n"
             elif index == int(testIndex) - 1:
-                test += "(" + str(entities_map[fromNode]) + "," + str(entities_map[toNode]) + ",0)],"
+                test += "(" + str(entities_map[fromNode]) + "," + str(entities_map[toNode]) + ","+str(edgeLabel)+")],"
             elif index < cntEdges-1:
-                valid += "(" + str(entities_map[fromNode]) + "," + str(entities_map[toNode]) + ",0),\n"
+                valid += "(" + str(entities_map[fromNode]) + "," + str(entities_map[toNode]) + ","+str(edgeLabel)+"),\n"
             else: # index == cntEdges-1
-                valid += "(" + str(entities_map[fromNode]) + "," + str(entities_map[toNode]) + ",0)]"
+                valid += "(" + str(entities_map[fromNode]) + "," + str(entities_map[toNode]) + ","+str(edgeLabel)+")]"
 
         for e in entities_set:
             entities += "u'" + str(e) + "'," + "\n"
         entities += "],"
+
+        if len(relations_set) == 0:
+            relations += "u'related_to', u'fake'],\n"
+        else:
+            for r in relations_set:
+                relations += "u'" + str(r) + "'," + "\n"
+            relations += "],"
 
     data = "{\n" + entities + relations + trains + test + valid + "}"
     with open(datafile +'.pkl','w') as fout:
